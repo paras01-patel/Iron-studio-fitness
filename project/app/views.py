fb7b5b2b6412d368efabfe855fc8b581bb20e736
paras01-patel <paraschandrawanshi649@gmail.com>
2026-05-13 15:00:52 +0530

    login

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib import messages
 
 # Create your views here.
 
 
-def base(req):
-    return render(req,'base.html')
+def home(req):
+    return render(req,'home.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # (demo check)
+        if username == "admin" and password == "123":
+
+            # 🔥 SESSION STORE
+            request.session['username'] = username
+            request.session['password'] = password   # (demo only)
+            request.session['is_logged_in'] = True
+
+            return redirect('home')
+
+        else:
+            return render(request, 'login.html', {'error': 'Invalid'})
+
+    return render(request, 'login.html')
